a7d7df47f1133b4fa8b7a806937856cc7877b2a9
Ali Tayyabi <ali@Alis-iMac.local>
2021-03-12 23:19:48 -0500

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/orders")
+@SpringBootApplication
+public class DemoApplication {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    @GetMapping
+    public List<Order> getOrder() {
+        return orderDao.getOrders().stream().
+                sorted(Comparator.comparing(Order::getId)).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        User user = new User(1, "joe mama", 10000);
+        System.out.println(user.getName());
+    }
+
+}
